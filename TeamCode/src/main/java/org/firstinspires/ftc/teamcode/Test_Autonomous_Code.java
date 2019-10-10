5aab4cac7b506f480d2fb36af8e346f920eb4e56
Sahana M <ssmoogi@gmail.com>
2019-10-10 15:17:47 -0700

    Create Test_Autonomous_Code.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_Autonomous_Code.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@Autonomous(name="Test_Autonomous_Code", group="Linear OpMode")
+
+public class Test_Autonomous_Code extends LinearOpMode{
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    DcMotor fldrive;
+    DcMotor frdrive;
+    DcMotor brdrive;
+    DcMotor bldrive;
+
+    double power= 0.5;
+
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Intialized");
+        telemetry.update();
+
+        fldrive = hardwareMap.dcMotor.get("fl_drive");
+        frdrive = hardwareMap.dcMotor.get("fr_drive");
+        brdrive = hardwareMap.dcMotor.get("br_drive");
+        bldrive = hardwareMap.dcMotor.get("bl_drive");
+
+        fldrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        frdrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        brdrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        bldrive.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+        runtime.reset();
+
+        fldrive.setPower(power);
+        frdrive.setPower(power);
+        brdrive.setPower(power);
+        bldrive.setPower(power);
+
+        sleep(2000);
+
+        power= 0.0;
+
+        fldrive.setPower(power);
+        frdrive.setPower(power);
+        brdrive.setPower(power);
+        bldrive.setPower(power);
+
+
+
+    }
+
+}
