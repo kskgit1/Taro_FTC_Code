aae217a65a13f6c7c9027ce14dd395f3d11d04c7
Sahana M <ssmoogi@gmail.com>
2019-10-10 18:53:59 -0700

    Update taro_tensorflow_experimentation.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/taro_tensorflow_experimentation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/taro_tensorflow_experimentation.java
@@ -52,7 +52,7 @@ import java.util.List;
  * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
  * is explained below.
  */
-@TeleOp(name = "Concept: TensorFlow Object Detection", group = "Concept")
+@TeleOp(name = "taro_tensorflow_experimentation", group = "Concept")
 //@Disabled
 public class taro_tensorflow_experimentation extends LinearOpMode {
     private static final String TFOD_MODEL_ASSET = "Skystone.tflite";
