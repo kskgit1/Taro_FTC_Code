61e684a61b601a90728cdb8a0f72be097de5c290
swedishfish_bball <44529969+kavirajesh22@users.noreply.github.com>
2019-10-26 10:12:59 -0700

    Update MecanumWheels.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumWheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumWheels.java
@@ -12,19 +12,23 @@ import com.qualcomm.robotcore.util.Range;
  
  @TeleOp(name="MecanumWheels", group="Linear Opmode")
  public class MecanumWheels extends LinearOpMode(){
+  
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor flDrive, frDrive, blDrive, brDrive;
+    
         double Speed = -gamepad1.left_stick_y;
         double Turn = gamepad1.left_stick_x;
         double Strafe = gamepad1.right_stick_x;
         double MAX_SPEED = 1.0;
         holonomic(Speed, Turn, Strafe, MAX_SPEED );
-    }
 
+    @Override
     public void holonomic(double Speed, double Turn, double Strafe, double MAX_SPEED){
 
-    LeftDrive = +Speed + Turn - Strafe;      
-    Left  = +Speed + Turn + Strafe;
-    Right Rear  = +Speed - Turn - Strafe
-    Right Front = +Speed - Turn + Strafe;
+    fldrive = +Speed + Turn - Strafe;      
+    frdrive  = +Speed + Turn + Strafe;
+    brdrive  = +Speed - Turn - Strafe
+    bldrive = +Speed - Turn + Strafe;
   
         double Magnitude = abs(Speed) + abs(Turn) + abs(Strafe);
         Magnitude = (Magnitude > 1) ? Magnitude : 1; //Set scaling to keep -1,+1 range
@@ -42,3 +46,4 @@ import com.qualcomm.robotcore.util.Range;
                     -Magnitude, +Magnitude, -MAX_SPEED, +MAX_SPEED));
         }
     }
+ }
