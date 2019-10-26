ffd7ff825f3fe1e7c5d6a4e80a40bf191e4db12e
swedishfish_bball <44529969+kavirajesh22@users.noreply.github.com>
2019-10-26 11:45:30 -0700

    Update MecanumWheels.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumWheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumWheels.java
@@ -1,49 +1,61 @@
- //edit
- 
- package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
- 
- 
- @TeleOp(name="MecanumWheels", group="Linear Opmode")
- public class MecanumWheels extends LinearOpMode(){
-  
+
+
+@TeleOp(name = "MecanumWheels", group = "Linear OpMode")
+public class MecanumWheels extends LinearOpMode {
+
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor flDrive, frDrive, blDrive, brDrive;
-    
-        double Speed = -gamepad1.left_stick_y;
-        double Turn = gamepad1.left_stick_x;
-        double Strafe = gamepad1.right_stick_x;
-        double MAX_SPEED = 1.0;
-        holonomic(Speed, Turn, Strafe, MAX_SPEED );
 
     @Override
-    public void holonomic(double Speed, double Turn, double Strafe, double MAX_SPEED){
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
 
-    fldrive = +Speed + Turn - Strafe;      
-    frdrive  = +Speed + Turn + Strafe;
-    brdrive  = +Speed - Turn - Strafe
-    bldrive = +Speed - Turn + Strafe;
-  
-        double Magnitude = abs(Speed) + abs(Turn) + abs(Strafe);
-        Magnitude = (Magnitude > 1) ? Magnitude : 1; //Set scaling to keep -1,+1 range
+        flDrive = hardwareMap.get(DcMotor.class, "fl_drive");
+        frDrive = hardwareMap.get(DcMotor.class, "fr_drive");
+        brDrive = hardwareMap.get(DcMotor.class, "br_drive");
+        blDrive = hardwareMap.get(DcMotor.class, "bl_drive");
 
-        leftFrontMotor.setPower(scale((scaleInput(Speed) + scaleInput(Turn) - scaleInput(Strafe)),
-                -Magnitude, +Magnitude, -MAX_SPEED, +MAX_SPEED));
-        if (leftRearMotor != null) {
-            leftRearMotor.setPower(scale((scaleInput(Speed) + scaleInput(Turn) + scaleInput(Strafe)),
-                    -Magnitude, +Magnitude, -MAX_SPEED, +MAX_SPEED));
-        }
-        rightFrontMotor.setPower(scale((scaleInput(Speed) - scaleInput(Turn) + scaleInput(Strafe)),
-                -Magnitude, +Magnitude, -MAX_SPEED, +MAX_SPEED));
-        if (rightRearMotor != null) {
-            rightRearMotor.setPower(scale((scaleInput(Speed) - scaleInput(Turn) - scaleInput(Strafe)),
-                    -Magnitude, +Magnitude, -MAX_SPEED, +MAX_SPEED));
+        flDrive.setDirection(DcMotor.Direction.FORWARD);
+        frDrive.setDirection(DcMotor.Direction.REVERSE);
+        brDrive.setDirection(DcMotor.Direction.REVERSE);
+        blDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive()) {
+
+            double Speed = -gamepad1.left_stick_y;
+            double Turn = gamepad1.left_stick_x;
+            double Strafe = gamepad1.right_stick_x;
+
+            double f_left;
+            double f_right;
+            double b_left;
+            double b_right;
+
+            f_left = Speed + Turn - Strafe;
+            f_right = Speed + Turn + Strafe;
+            b_right = Speed - Turn - Strafe;
+            b_left = Speed - Turn + Strafe;
+
+            flDrive.setPower(Range.clip(f_left, -1.0, 1.0));
+            frDrive.setPower(Range.clip(f_right, -1.0, 1.0));
+            brDrive.setPower(Range.clip(b_right, -1.0, 1.0));
+            blDrive.setPower(Range.clip(b_left, -1.0, 1.0));
+
+
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Motors", "left (%.2f), right (%.2f)", f_left, f_right, b_left, b_right);
+            telemetry.update();
         }
     }
- }
+}
