8cc67cdea2fe4278f887b782298bc69db842f796
swedishfish_bball <44529969+kavirajesh22@users.noreply.github.com>
2019-10-26 11:47:18 -0700

    Update left.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/left.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/left.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
 @Autonomous(name="left", group="Linear OpMode")
 @Disabled
