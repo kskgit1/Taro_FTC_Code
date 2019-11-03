4f50b32f6ebfed78593fdbcf289381c9a32bf512
Sahana M <ssmoogi@gmail.com>
2019-11-03 14:45:25 -0800

    Update actual_servo_test.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/actual_servo_test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/actual_servo_test.java
@@ -48,9 +48,9 @@ import com.qualcomm.robotcore.hardware.Servo;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-@TeleOp(name = "servo_test", group = "Linear OpMode")
+@TeleOp(name = "actual_servo_test", group = "Linear OpMode")
 
-public class servo_test extends LinearOpMode {
+public class actual_servo_test extends LinearOpMode {
 
     static final double INCREMENT   = 0.03;     // amount to slew servo each CYCLE_MS cycle
     static final int    CYCLE_MS    =   50;     // period of each cycle
