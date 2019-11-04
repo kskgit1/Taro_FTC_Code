ddf36e14a0f798b955ba1ed4f67bc202462696dc
Sahana M <ssmoogi@gmail.com>
2019-11-03 16:45:13 -0800

    Update DriveWithEncoder.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveWithEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveWithEncoder.java
@@ -29,12 +29,15 @@ public class DriveWithEncoder extends LinearOpMode
 
         // reset encoder count kept by left motor.
         flDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        flDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        flDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        flDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        // set left motor to run to target encoder position and stop with brakes on.
-        leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        blDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        brDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        
+        // set motors to run to target encoder position and stop with brakes on.
+        flDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        blDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        brDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         // set right motor to run without regard to an encoder.
         //rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -49,29 +52,19 @@ public class DriveWithEncoder extends LinearOpMode
         telemetry.addData("Mode", "running");
         telemetry.update();
 
-        // set left motor to run to 5000 encoder counts.
+        //set left motor to run to 5000 encoder counts.
 
-        leftMotor.setTargetPosition(5000);
-        
-        // set both motors to 25% power. Movement will start.
-
-        leftMotor.setPower(0.25);
-        rightMotor.setPower(0.25);
+        //leftMotor.setTargetPosition(5000);
 
         // wait while opmode is active and left motor is busy running to position.
 
-        while (opModeIsActive() && leftMotor.isBusy())   
+        while (opModeIsActive() && leftMotor.isBusy())
         {
             telemetry.addData("encoder-fwd", leftMotor.getCurrentPosition() + "  busy=" + leftMotor.isBusy());
             telemetry.update();
             idle();
         }
 
-        // set motor power to zero to turn off motors. The motors stop on their own but
-        // power is still applied so we turn off the power.
-
-        leftMotor.setPower(0.0);
-        rightMotor.setPower(0.0);
 
         // wait 5 sec so you can observe the final encoder position.
 
