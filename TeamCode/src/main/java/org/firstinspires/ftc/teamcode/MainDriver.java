c6c4c30a711dbac4a118060339b96d61634fd3c4
Sahana M <ssmoogi@gmail.com>
2019-11-07 20:48:48 -0800

    Update MainDriver.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainDriver.java
@@ -14,15 +14,8 @@ public class MainDriver extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor flDrive, frDrive, blDrive, brDrive, flyWheel, back_Slide, left_Slide, top_Slide;
     Servo arm_servo, hand_servo, head_servo, hair_1, hair_2;
-    int currentposition = 0;
-
-    /*public void release(double currentposition)
-    {
-        arm_bottom_servo.setPosition(currentposition);
-    }
-    public void grab(double currentposition)
-    {    // if arm is open, make it closed
-    }*/
+    double currentposition_arm = 0;
+    double currentposition_hand = 0;
 
 
     @Override
@@ -33,26 +26,26 @@ public class MainDriver extends LinearOpMode {
         arm_servo = hardwareMap.get(Servo.class, "arm");
         hand_servo = hardwareMap.get(Servo.class, "hand");
         head_servo = hardwareMap.get(Servo.class, "head");
-        clamp_1 = hardwareMap.get(Servo.class, "hair1");
-        clamp_2 = hardwareMap.get(Servo.class, "hair2");
+        hair_1 = hardwareMap.get(Servo.class, "hair1");
+        hair_2 = hardwareMap.get(Servo.class, "hair2");
 
         flDrive = hardwareMap.get(DcMotor.class, "fl_drive");
         frDrive = hardwareMap.get(DcMotor.class, "fr_drive");
         brDrive = hardwareMap.get(DcMotor.class, "br_drive");
         blDrive = hardwareMap.get(DcMotor.class, "bl_drive");
         flyWheel = hardwareMap.get(DcMotor.class, "fly_Wheel");
-        backSlide = hardwareMap.get(DcMotor.class, "back_Slide");
-        leftSlide = hardwareMap.get(DcMotor.class, "left_Slide");
-        topSLide = hardwareMap.get(DcMotor.class, "top_Slide");
+        back_Slide = hardwareMap.get(DcMotor.class, "back_Slide");
+        left_Slide = hardwareMap.get(DcMotor.class, "left_Slide");
+        top_Slide = hardwareMap.get(DcMotor.class, "top_Slide");
 
         flDrive.setDirection(DcMotor.Direction.FORWARD);
         frDrive.setDirection(DcMotor.Direction.REVERSE);
         brDrive.setDirection(DcMotor.Direction.REVERSE);
         blDrive.setDirection(DcMotor.Direction.FORWARD);
         flyWheel.setDirection(DcMotor.Direction.FORWARD);
-        backSlide.setDirection(DcMotor.Direction.FORWARD);
-        leftSlide.setDirection(DcMotor.Direction.FORWARD);
-        topSlide.setDirection(DcMotor.Direction.FORWARD);
+        back_Slide.setDirection(DcMotor.Direction.FORWARD);
+        left_Slide.setDirection(DcMotor.Direction.FORWARD);
+        top_Slide.setDirection(DcMotor.Direction.FORWARD);
 
         waitForStart();
         runtime.reset();
@@ -70,48 +63,60 @@ public class MainDriver extends LinearOpMode {
             double f_right;
             double b_left;
             double b_right;
-            double back_Slide;
-            double left_Slide;
-            double top_Slide;
+            double back_slide;
+            double left_slide;
+            double top_slide;
 
             f_left = Speed + Turn + Strafe;
             f_right = Speed - Turn - Strafe;
             b_right = Speed - Turn + Strafe;
             b_left = Speed + Turn - Strafe;
-            back_Slide = Lift;
-            left_Slide = Lift;
-            top_Slide = Place;
+            back_slide = Lift;
+            left_slide = Lift;
+            top_slide = Place;
 
             flDrive.setPower(Range.clip(f_left, -1.0, 1.0));
             frDrive.setPower(Range.clip(f_right, -1.0, 1.0));
             brDrive.setPower(Range.clip(b_right, -1.0, 1.0));
             blDrive.setPower(Range.clip(b_left, -1.0, 1.0));
-            backSlide.setPower(Range.clip(back_slide, -1.0, 1.0));
-            leftSlide.setPower(Range.clip(left_slide, -1.0, 1.0));
-            topSlide.setPower(Range.clip(top_slide, -1.0, 1.0));
+            back_Slide.setPower(Range.clip(back_slide, -1.0, 1.0));
+            left_Slide.setPower(Range.clip(left_slide, -1.0, 1.0));
+            top_Slide.setPower(Range.clip(top_slide, -1.0, 1.0));
             
 
             if(gamepad1.left_bumper)
             {
-                currentposition -= - 0.1;
-                arm_servo.setPosition(currentposition);
+                currentposition_arm = currentposition_arm - 0.1;
+                arm_servo.setPosition(currentposition_arm);
             }
 
             if(gamepad1.right_bumper)
             {
-                currentposition += 0.1;
-                arm_servo.setPosition(currentposition);
+                currentposition_arm = currentposition_arm + 0.1;
+                arm_servo.setPosition(currentposition_arm);
             }
 
-           /* if(gamepad2.x)
+            if(gamepad1.dpad_down)
             {
-               flydrive.setPower(0.7); 
+                currentposition_arm = currentposition_arm - 0.1;
+                arm_servo.setPosition(currentposition_arm);
+            }
+
+            if(gamepad1.dpad_up)
+            {
+                currentposition_arm = currentposition_arm + 0.1;
+                arm_servo.setPosition(currentposition_arm);
+            }
+
+            while(gamepad2.x)
+            {
+               flyWheel.setPower(0.7);
             }
             
-             if(gamepad2.b)
+             if(gamepad2.y)
             {
-               flydrive.setPower(0.0); 
-            }*/
+               flyWheel.setPower(0.0);
+            }
 
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Motors", "left (%.2f), right (%.2f)", f_left, f_right, b_left, b_right);
