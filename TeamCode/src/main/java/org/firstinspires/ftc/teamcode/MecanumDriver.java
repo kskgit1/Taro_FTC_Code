ed578c9679005515ed97a10b044c03fc07d925f4
Sahana M <ssmoogi@gmail.com>
2019-11-07 17:06:21 -0800

    Update MecanumDriver.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriver.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
 @TeleOp(name = "MecanumDriver", group = "Linear OpMode")
@@ -12,11 +13,27 @@ public class MecanumDriver extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor flDrive, frDrive, blDrive, brDrive, flyDrive;
+    Servo arm_bottom_servo;
+    int increment = 0;
+
+    /*public void release(double currentposition)
+    {
+
+        arm_bottom_servo.setPosition(currentposition);
+    }
+
+    public void grab(double currentposition)
+    {    // if arm is open, make it closed
+
+    }*/
+
 
     @Override
     public void runOpMode() {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
+
+        arm_bottom_servo = hardwareMap.get(Servo.class, "arm");
 
         flDrive = hardwareMap.get(DcMotor.class, "fl_drive");
         frDrive = hardwareMap.get(DcMotor.class, "fr_drive");
@@ -54,6 +71,19 @@ public class MecanumDriver extends LinearOpMode {
             brDrive.setPower(Range.clip(b_right, -1.0, 1.0));
             blDrive.setPower(Range.clip(b_left, -1.0, 1.0));
             
+
+            if(gamepad1.left_bumper)
+            {
+                increment -= - 0.1;
+                arm_bottom_servo.setPosition(increment);
+            }
+
+            if(gamepad1.right_bumper)
+            {
+                increment += 0.1;
+                arm_bottom_servo.setPosition(increment);
+            }
+
            /* if(gamepad2.x)
             {
                flydrive.setPower(0.7); 
@@ -64,10 +94,11 @@ public class MecanumDriver extends LinearOpMode {
                flydrive.setPower(0.0); 
             }*/
 
-
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Motors", "left (%.2f), right (%.2f)", f_left, f_right, b_left, b_right);
             telemetry.update();
         }
+
+
     }
 }
