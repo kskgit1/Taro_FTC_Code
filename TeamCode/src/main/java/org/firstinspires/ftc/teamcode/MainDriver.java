630580ad44fb6b240ae07fe41127799b9809868d
swedishfish_bball <44529969+kavirajesh22@users.noreply.github.com>
2019-11-08 08:56:46 -0800

    Update MainDriver.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainDriver.java
@@ -12,11 +12,13 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class MainDriver extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor flDrive, frDrive, blDrive, brDrive, flyWheel, back_Slide, left_Slide, top_Slide; // initialize all motors
-    Servo arm_servo, hand_servo, head_servo, hair_1, hair_2; // initialize all servos
+    private DcMotor flDrive, frDrive, blDrive, brDrive, fly_Wheel, back_Slide, left_Slide, top_Slide; // initialize all motors
+    Servo arm_servo, hand_servo, head_servo, hair1_servo, hair2_servo; // initialize all servos
     double currentposition_arm = 0;  // currentpositions for arm, hand, and head servos
     double currentposition_hand = 0;
     double currentposition_head = 0;
+    double currentposition_hair1 = 0;
+    double currentposition_hair2 = 0;
 
 
     @Override
@@ -34,7 +36,7 @@ public class MainDriver extends LinearOpMode {
         frDrive = hardwareMap.get(DcMotor.class, "fr_drive");
         brDrive = hardwareMap.get(DcMotor.class, "br_drive");
         blDrive = hardwareMap.get(DcMotor.class, "bl_drive");
-        flyWheel = hardwareMap.get(DcMotor.class, "fly_Wheel");
+        fly_Wheel = hardwareMap.get(DcMotor.class, "fly_Wheel");
         back_Slide = hardwareMap.get(DcMotor.class, "back_Slide");
         left_Slide = hardwareMap.get(DcMotor.class, "left_Slide");
         top_Slide = hardwareMap.get(DcMotor.class, "top_Slide");
@@ -43,7 +45,7 @@ public class MainDriver extends LinearOpMode {
         frDrive.setDirection(DcMotor.Direction.REVERSE);
         brDrive.setDirection(DcMotor.Direction.REVERSE);
         blDrive.setDirection(DcMotor.Direction.FORWARD);
-        flyWheel.setDirection(DcMotor.Direction.FORWARD);
+        fly_Wheel.setDirection(DcMotor.Direction.FORWARD);
         back_Slide.setDirection(DcMotor.Direction.FORWARD);
         left_Slide.setDirection(DcMotor.Direction.FORWARD);
         top_Slide.setDirection(DcMotor.Direction.FORWARD);
@@ -56,6 +58,7 @@ public class MainDriver extends LinearOpMode {
             double Speed = -gamepad1.left_stick_y;
             double Turn = gamepad1.right_stick_x;
             double Strafe = gamepad1.left_stick_x;
+            double Catch = gamepad1.right_trigger;
             double Lift = gamepad2.left_stick_y;    // using linear slides
             double Place = gamepad2.right_stick_x;  // using top slide
           
@@ -64,6 +67,7 @@ public class MainDriver extends LinearOpMode {
             double f_right;
             double b_left;
             double b_right;
+            double fly_wheel;
             double back_slide;
             double left_slide;
             double top_slide;
@@ -72,6 +76,7 @@ public class MainDriver extends LinearOpMode {
             f_right = Speed - Turn - Strafe;
             b_right = Speed - Turn + Strafe;
             b_left = Speed + Turn - Strafe;
+            fly_Wheel = Catch;
             back_slide = Lift;
             left_slide = Lift;
             top_slide = Place;
@@ -80,31 +85,19 @@ public class MainDriver extends LinearOpMode {
             frDrive.setPower(Range.clip(f_right, -1.0, 1.0));
             brDrive.setPower(Range.clip(b_right, -1.0, 1.0));
             blDrive.setPower(Range.clip(b_left, -1.0, 1.0));
+            fly_Wheel.setPower(Range.clip(fly_wheel, -1.0, 1.0));
             back_Slide.setPower(Range.clip(back_slide, -1.0, 1.0));
             left_Slide.setPower(Range.clip(left_slide, -1.0, 1.0));
             top_Slide.setPower(Range.clip(top_slide, -1.0, 1.0));
             
-            
-            while(gamepad1.a)  // toggle switch for flywheel motor
-            {
-               if (flyWheel.setPower(0.7));
-                {
-                    flyWheel.setPower(0.0);   
-                }
-                else 
-                {
-                    flyWheel.setPower(0.7);
-                }
-            }
-            
 
-            if(gamepad1.dpad_down) // reduce servo position for arm
+            if(gamepad1.a) // reduce servo position for arm
             {
                 currentposition_arm = currentposition_arm - 0.1;
                 arm_servo.setPosition(currentposition_arm);
             }
 
-            if(gamepad1.dpad_up  // increase servo position for arm
+            if(gamepad1.x)  // increase servo position for arm
             {
                 currentposition_arm = currentposition_arm + 0.1;
                 arm_servo.setPosition(currentposition_arm);
@@ -137,11 +130,29 @@ public class MainDriver extends LinearOpMode {
             
             if(gamepad2.a)   // clamp both servos at same time
             {
-                  hair1_servo.setPosition(0.5);
+                if(currentposition_hair1 = 0)
+                {
+                    currentposition_hair1 = 0.5;
+                    hair1_servo.setPosition(currentposition_hair1);
+                }
+                else
+                {
+                    currentposition_hair1 = 0;
+                    hair1_servo.setPosition(currentposition_hair1);
             }
             if(gamepad2.a)
             {
-                  hair2_servo.setPosition(0.5);
+                if(currentposition_hair2 = 0)
+                {
+                    currentpostion_hair2 = 0.5;
+                    hair2_servo.setPosition(currentposition_hair2);
+                }
+                else
+                {
+                    currentposition_hair2 = 0;
+                    hair_2servo.setPosition(currentposition_hair2);
+                }
+                
             }
             
 
