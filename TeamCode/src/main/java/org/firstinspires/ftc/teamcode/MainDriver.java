27ec94fe0611958e8bc9b65b916052c654b3b836
Sahana M <ssmoogi@gmail.com>
2019-11-08 19:54:22 -0800

    Update MainDriver.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainDriver.java
@@ -61,7 +61,7 @@ public class MainDriver extends LinearOpMode {
             double Catch = gamepad1.right_trigger;
             double Lift = gamepad2.left_stick_y;    // using linear slides
             double Place = gamepad2.right_stick_x;  // using top slide
-          
+
 
             double f_left;
             double f_right;
@@ -76,7 +76,7 @@ public class MainDriver extends LinearOpMode {
             f_right = Speed - Turn - Strafe;
             b_right = Speed - Turn + Strafe;
             b_left = Speed + Turn - Strafe;
-            fly_Wheel = Catch;
+            fly_wheel = Catch;
             back_slide = Lift;
             left_slide = Lift;
             top_slide = Place;
@@ -89,78 +89,72 @@ public class MainDriver extends LinearOpMode {
             back_Slide.setPower(Range.clip(back_slide, -1.0, 1.0));
             left_Slide.setPower(Range.clip(left_slide, -1.0, 1.0));
             top_Slide.setPower(Range.clip(top_slide, -1.0, 1.0));
-            
 
-            if(gamepad1.a) // reduce servo position for arm
+
+            if (gamepad1.a) // reduce servo position for arm
             {
                 currentposition_arm = currentposition_arm - 0.1;
                 arm_servo.setPosition(currentposition_arm);
             }
 
-            if(gamepad1.x)  // increase servo position for arm
+            if (gamepad1.x)  // increase servo position for arm
             {
                 currentposition_arm = currentposition_arm + 0.1;
                 arm_servo.setPosition(currentposition_arm);
             }
 
-            if(gamepad1.left_bumper)  // reduce servo position for hand
+            if (gamepad1.left_bumper)  // reduce servo position for hand
             {
                 currentposition_hand = currentposition_hand - 0.1;
                 hand_servo.setPosition(currentposition_hand);
             }
 
-            if(gamepad1.right_bumper) // increase servo position for hand
+            if (gamepad1.right_bumper) // increase servo position for hand
             {
                 currentposition_hand = currentposition_hand + 0.1;
                 hand_servo.setPosition(currentposition_hand);
             }
 
-            if(gamepad2.left_bumper)  // turn head servo one way
+            if (gamepad2.left_bumper)  // turn head servo one way
             {
                 currentposition_head = currentposition_head - 0.1;
                 head_servo.setPosition(currentposition_hand);
             }
-            
-            if(gamepad2.right_bumper)  // turn head servo other way
+
+            if (gamepad2.right_bumper)  // turn head servo other way
             {
                 currentposition_head = currentposition_head + 0.1;
                 head_servo.setPosition(currentposition_head);
-                
+
             }
-            
-            if(gamepad2.a)   // clamp both servos at same time
+
+            if (gamepad2.a)   // clamp both servos at same time
             {
-                if(currentposition_hair1 = 0)
-                {
+                if (currentposition_hair1 == 0) {
                     currentposition_hair1 = 0.5;
                     hair1_servo.setPosition(currentposition_hair1);
-                }
-                else
-                {
+                } else {
                     currentposition_hair1 = 0;
                     hair1_servo.setPosition(currentposition_hair1);
-            }
-            if(gamepad2.a)
-            {
-                if(currentposition_hair2 = 0)
-                {
-                    currentpostion_hair2 = 0.5;
-                    hair2_servo.setPosition(currentposition_hair2);
                 }
-                else
-                {
-                    currentposition_hair2 = 0;
-                    hair_2servo.setPosition(currentposition_hair2);
-                }
-                
-            }
-            
+                if (gamepad2.a) {
+                    if (currentposition_hair2 == 0) {
+                        currentposition_hair2 = 0.5;
+                        hair2_servo.setPosition(currentposition_hair2);
+                    } else {
+                        currentposition_hair2 = 0;
+                        hair2_servo.setPosition(currentposition_hair2);
+                    }
 
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Motors", "left (%.2f), right (%.2f)", f_left, f_right, b_left, b_right);
-            telemetry.update();
+                }
+
+
+                telemetry.addData("Status", "Run Time: " + runtime.toString());
+                telemetry.addData("Motors", "left (%.2f), right (%.2f)", f_left, f_right, b_left, b_right);
+                telemetry.update();
+            }
+
+
         }
-
-
     }
 }
