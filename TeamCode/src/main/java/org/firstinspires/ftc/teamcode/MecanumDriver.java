fcb58cc639a01264e14adc8397dd1921c0008173
Sahana M <ssmoogi@gmail.com>
2019-12-01 14:41:38 -0800

    Update MecanumDriver.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriver.java
@@ -12,7 +12,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class MecanumDriver extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor flDrive, frDrive, blDrive, brDrive, flyDrive;
+    private DcMotor flDrive, frDrive, blDrive, brDrive;
     //Servo arm_bottom_servo;
 
     @Override
@@ -24,13 +24,11 @@ public class MecanumDriver extends LinearOpMode {
         frDrive = hardwareMap.get(DcMotor.class, "fr_drive");
         brDrive = hardwareMap.get(DcMotor.class, "br_drive");
         blDrive = hardwareMap.get(DcMotor.class, "bl_drive");
-        //flyDrive = hardwareMap.get(DcMotor.class, "fly_drive");
 
         flDrive.setDirection(DcMotor.Direction.FORWARD);
         frDrive.setDirection(DcMotor.Direction.REVERSE);
         brDrive.setDirection(DcMotor.Direction.REVERSE);
         blDrive.setDirection(DcMotor.Direction.FORWARD);
-        //flyDrive.setDirection(DcMotor.Direction.FORWARD);
 
         waitForStart();
         runtime.reset();
@@ -40,26 +38,22 @@ public class MecanumDriver extends LinearOpMode {
             double Speed = -gamepad1.left_stick_y;
             double Turn = gamepad1.right_stick_x;
             double Strafe = gamepad1.left_stick_x;
-            double Fly = gamepad1.right_trigger;
 
             double f_left;
             double f_right;
             double b_left;
             double b_right;
-            double fly;
 
             f_left = Speed + Turn + Strafe;
             f_right = Speed - Turn - Strafe;
             b_right = Speed - Turn + Strafe;
             b_left = Speed + Turn - Strafe;
-            fly = Fly;
 
             flDrive.setPower(Range.clip(f_left, -1.0, 1.0));
             frDrive.setPower(Range.clip(f_right, -1.0, 1.0));
             brDrive.setPower(Range.clip(b_right, -1.0, 1.0));
             blDrive.setPower(Range.clip(b_left, -1.0, 1.0));
 
-            
 
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Motors", "left (%.2f), right (%.2f)", f_left, f_right, b_left, b_right);
