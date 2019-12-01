9f83c0809e3d9d640870d2f121dfc6684822e9c4
Sahana M <ssmoogi@gmail.com>
2019-12-01 14:36:52 -0800

    Update MecanumDriver.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriver.java
@@ -13,27 +13,12 @@ public class MecanumDriver extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor flDrive, frDrive, blDrive, brDrive, flyDrive;
-    Servo arm_bottom_servo;
-    int increment = 0;
-
-    /*public void release(double currentposition)
-    {
-
-        arm_bottom_servo.setPosition(currentposition);
-    }
-
-    public void grab(double currentposition)
-    {    // if arm is open, make it closed
-
-    }*/
-
+    //Servo arm_bottom_servo;
 
     @Override
     public void runOpMode() {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
-
-        arm_bottom_servo = hardwareMap.get(Servo.class, "arm");
 
         flDrive = hardwareMap.get(DcMotor.class, "fl_drive");
         frDrive = hardwareMap.get(DcMotor.class, "fr_drive");
@@ -55,44 +40,26 @@ public class MecanumDriver extends LinearOpMode {
             double Speed = -gamepad1.left_stick_y;
             double Turn = gamepad1.right_stick_x;
             double Strafe = gamepad1.left_stick_x;
+            double Fly = gamepad1.right_trigger;
 
             double f_left;
             double f_right;
             double b_left;
             double b_right;
+            double fly;
 
             f_left = Speed + Turn + Strafe;
             f_right = Speed - Turn - Strafe;
             b_right = Speed - Turn + Strafe;
             b_left = Speed + Turn - Strafe;
+            fly = Fly;
 
             flDrive.setPower(Range.clip(f_left, -1.0, 1.0));
             frDrive.setPower(Range.clip(f_right, -1.0, 1.0));
             brDrive.setPower(Range.clip(b_right, -1.0, 1.0));
             blDrive.setPower(Range.clip(b_left, -1.0, 1.0));
+
             
-
-            if(gamepad1.left_bumper)
-            {
-                increment -= - 0.1;
-                arm_bottom_servo.setPosition(increment);
-            }
-
-            if(gamepad1.right_bumper)
-            {
-                increment += 0.1;
-                arm_bottom_servo.setPosition(increment);
-            }
-
-           /* if(gamepad2.x)
-            {
-               flydrive.setPower(0.7); 
-            }
-            
-             if(gamepad2.b)
-            {
-               flydrive.setPower(0.0); 
-            }*/
 
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Motors", "left (%.2f), right (%.2f)", f_left, f_right, b_left, b_right);
