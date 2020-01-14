17046f8bafee10b962c67d48dd6ba0173ac46d60
FTC Team Taro <57720413+team-taro@users.noreply.github.com>
2020-01-13 16:29:57 -0800

    Update MecanumAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumAuto.java
@@ -2,10 +2,12 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+
 
 //this is the official Autonomous Code for Team Taro.
 //this uses Mecanum wheels. They are a bit different from normal wheels: not only can they turn and move forward/backward,
@@ -22,98 +24,9 @@ public class MecanumAuto extends LinearOpMode{
     DcMotor frdrive; //motor 1
     DcMotor brdrive; //motor 2
     DcMotor bldrive; //motor 3
+    //DcMotor flywheel; //motor for flywheels
     
-    //functions we will call into the main program
-    //move forward function
-    public void forward(double power, long wait_time) {
-        fldrive.setPower(power);
-        frdrive.setPower(power);
-        brdrive.setPower(power);
-        bldrive.setPower(power);
-        sleep(wait_time);
-        power = 0.0;
-        //"stop"
-        fldrive.setPower(power);
-        frdrive.setPower(power);
-        brdrive.setPower(power);
-        bldrive.setPower(power);
-    }
 
-    //move backward function
-    public void backward(double power, long wait_time) {
-        fldrive.setPower(-power);
-        frdrive.setPower(-power);
-        brdrive.setPower(-power);
-        bldrive.setPower(-power);
-        sleep(wait_time);
-        power = 0.0;
-        //"stop"
-        fldrive.setPower(-power);
-        frdrive.setPower(-power);
-        brdrive.setPower(-power);
-        bldrive.setPower(-power);
-    }
-
-    //turn left function
-    public void left(double power, long wait_time) {
-        fldrive.setPower(-power);
-        frdrive.setPower(power);
-        brdrive.setPower(power);
-        bldrive.setPower(-power);
-        sleep(wait_time);
-        power = 0.0;
-        //"stop"
-        fldrive.setPower(-power);
-        frdrive.setPower(-power);
-        brdrive.setPower(-power);
-        bldrive.setPower(-power);
-    }
-
-    //strafe right function
-    public void right(double power, long wait_time) {
-        fldrive.setPower(power);
-        frdrive.setPower(-power);
-        brdrive.setPower(-power);
-        bldrive.setPower(power);
-        sleep(wait_time);
-        power = 0.0;
-        //"stop"
-        fldrive.setPower(-power);
-        frdrive.setPower(-power);
-        brdrive.setPower(-power);
-        bldrive.setPower(-power);
-    }
-    
-    //strafe left function
-    public void strafeleft(double power, long wait_time) {
-        fldrive.setPower(-power);
-        frdrive.setPower(power);
-        brdrive.setPower(-power);
-        bldrive.setPower(power);
-        sleep(wait_time);
-        power = 0.0;
-        //"stop"
-        fldrive.setPower(power);
-        frdrive.setPower(power);
-        brdrive.setPower(power);
-        bldrive.setPower(power);
-    }
-    
-    //strafe right function
-    public void straferight(double power, long wait_time) {
-        fldrive.setPower(power);
-        frdrive.setPower(-power);
-        brdrive.setPower(power);
-        bldrive.setPower(-power);
-        sleep(wait_time);
-        power = 0.0;
-        //"stop"
-        fldrive.setPower(power);
-        frdrive.setPower(power);
-        brdrive.setPower(power);
-        bldrive.setPower(power);
-    }
-    
     //main program
     public void runOpMode() throws InterruptedException {
         telemetry.addData("Status", "Intialized");
@@ -124,27 +37,259 @@ public class MecanumAuto extends LinearOpMode{
         frdrive  = hardwareMap.get(DcMotor.class, "fr_drive");
         brdrive = hardwareMap.get(DcMotor.class, "br_drive");
         bldrive = hardwareMap.get(DcMotor.class, "bl_drive");
+        //flywheel = hardwareMap.get(DcMotor.class, "fly_wheel")
+
 
         //set the direction of each of the motors
         fldrive.setDirection(DcMotor.Direction.FORWARD);
         frdrive.setDirection(DcMotor.Direction.REVERSE);
         brdrive.setDirection(DcMotor.Direction.REVERSE);
         bldrive.setDirection(DcMotor.Direction.FORWARD);
+        //flywheel.setDirection(DcMotor.Direction.REVERSE)
 
         //wait for the START button to be pressed
         waitForStart();
         runtime.reset();
 
-        //call the functions: power, distance
-        forward(0.5, 300);
-        right(0.5, 500);
-        forward(0.5, 2500);
-        //right(0.5, 1000);
-        //strafeleft(0.5,1000);
-        //straferight(0.5,1000);
+
 
 
     }
-   
 
+    //functions we will call into the main program
+
+    /*
+    //using fly wheels function
+    public void flywheels(double flypower, int flytime)
+    {
+        //set power
+        flyWheel.setPower(flypower);
+
+        //rotate for amount of time
+        sleep(flytime);
+
+        //stop fly wheels
+        flypower = 0.0;
+
+        flyWheel.setPower(flypower);
+    }
+    */
+
+    //driving and strafing functions below
+
+    //drive forward using encoders
+    public void driveforward(double drivepower, int distance)
+    {
+        //reset encoders
+        fldrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frdrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        brdrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bldrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //set target distance
+        fldrive.setTargetPosition(distance);
+        frdrive.setTargetPosition(distance);
+        brdrive.setTargetPosition(distance);
+        bldrive.setTargetPosition(distance);
+
+        //Set to run to position
+        fldrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frdrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        brdrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        bldrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        //set drive power
+        forward(drivepower);
+
+        //stop driving and set modes back to normal
+        stopdriving();
+        fldrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frdrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        brdrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bldrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    //used in driving forward using encoders
+    public void forward(double drivepower)
+    {
+        fldrive.setPower(-drivepower);
+        frdrive.setPower(-drivepower);
+        brdrive.setPower(-drivepower);
+        bldrive.setPower(-drivepower);
+    }
+
+    //drive backward using encoders
+    public void drivebackward(double drivepower, int distance)
+    {
+        //reset encoders
+        fldrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frdrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        brdrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bldrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //set target distance
+        fldrive.setTargetPosition(distance);
+        frdrive.setTargetPosition(distance);
+        brdrive.setTargetPosition(distance);
+        bldrive.setTargetPosition(distance);
+
+        //Set to run to position
+        fldrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frdrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        brdrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        bldrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        //set drive power
+        backward(drivepower);
+
+        //stop driving and set modes back to normal
+        stopdriving();
+        fldrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frdrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        brdrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bldrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    //used in driving backward using encoders
+    public void backward(double drivepower)
+    {
+        fldrive.setPower(-drivepower);
+        frdrive.setPower(-drivepower);
+        brdrive.setPower(-drivepower);
+        bldrive.setPower(-drivepower);
+    }
+
+    //strafe left using encoders
+    public void drivestrafeleft(double drivepower, int distance)
+    {
+        //reset encoders
+        fldrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frdrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        brdrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bldrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //set target distance
+        fldrive.setTargetPosition(distance);
+        frdrive.setTargetPosition(distance);
+        brdrive.setTargetPosition(distance);
+        bldrive.setTargetPosition(distance);
+
+        //Set to run to position
+        fldrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frdrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        brdrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        bldrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        //set drive power
+        strafeleft(drivepower);
+
+        //stop driving and set modes back to normal
+        stopdriving();
+        fldrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frdrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        brdrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bldrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    //used in strafing left using encoders
+    public void strafeleft(double drivepower)
+    {
+        fldrive.setPower(-drivepower);
+        frdrive.setPower(drivepower);
+        brdrive.setPower(-drivepower);
+        bldrive.setPower(drivepower);
+    }
+
+    //strafe right using encoders
+    public void drivestraferight(double drivepower, int distance)
+    {
+        //reset encoders
+        fldrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frdrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        brdrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bldrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //set target distance
+        fldrive.setTargetPosition(distance);
+        frdrive.setTargetPosition(distance);
+        brdrive.setTargetPosition(distance);
+        bldrive.setTargetPosition(distance);
+
+        //Set to run to position
+        fldrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frdrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        brdrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        bldrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        //set drive power
+        straferight(drivepower);
+
+        //stop driving and set modes back to normal
+        stopdriving();
+        fldrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frdrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        brdrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bldrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    //used in strafing right using encoders
+    public void straferight(double drivepower)
+    {
+        fldrive.setPower(drivepower);
+        frdrive.setPower(-drivepower);
+        brdrive.setPower(drivepower);
+        bldrive.setPower(-drivepower);
+    }
+
+    //used in driving and strafing using encoders
+    public void stopdriving()
+    {
+        fldrive.setPower(0);
+        frdrive.setPower(0);
+        brdrive.setPower(0);
+        bldrive.setPower(0);
+    }
+
+    //rotate left function
+    public void rotateleft(double rotatepower, long rotatetime)
+    {
+        //set power
+        fldrive.setPower(-rotatepower);
+        frdrive.setPower(rotatepower);
+        brdrive.setPower(rotatepower);
+        bldrive.setPower(-rotatepower);
+
+        //rotate for amount of time
+        sleep(rotatetime);
+
+        //stop rotating
+        rotatepower = 0.0;
+
+        fldrive.setPower(rotatepower);
+        frdrive.setPower(rotatepower);
+        brdrive.setPower(rotatepower);
+        bldrive.setPower(rotatepower);
+    }
+
+    //rotate right function
+    public void rotateright(double rotatepower, long rotatetime)
+    {
+        //set power
+        fldrive.setPower(rotatepower);
+        frdrive.setPower(-rotatepower);
+        brdrive.setPower(-rotatepower);
+        bldrive.setPower(rotatepower);
+
+        //rotate for amount of time
+        sleep(rotatetime);
+
+        //stop rotating
+        rotatepower = 0.0;
+
+        fldrive.setPower(rotatepower);
+        frdrive.setPower(rotatepower);
+        brdrive.setPower(rotatepower);
+        bldrive.setPower(rotatepower);
+    }
 }
+
