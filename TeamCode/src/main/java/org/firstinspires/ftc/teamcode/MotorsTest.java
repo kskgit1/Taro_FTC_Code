7cd1fcd653be3d4ec9ff557e2681eec0a1320be6
kskgit1 <55639416+kskgit1@users.noreply.github.com>
2020-01-31 19:47:44 -0800

    Update MotorsTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorsTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorsTest.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
@@ -10,8 +11,8 @@ import com.qualcomm.robotcore.util.Range;
 public class MotorsTest extends LinearOpMode
 {
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor flywheel, leftslide, rightslide, arm;
-    double flypower, armpower;
+    private DcMotor flywheel1, flywheel2, leftslide, rightslide;
+    double flypower;
     double liftpower = gamepad1.left_stick_y;
     int liftincrement = 500;
 
@@ -19,21 +20,15 @@ public class MotorsTest extends LinearOpMode
     @Override
     public void runOpMode()
     {
-        flywheel = hardwareMap.get(DcMotor.class, "flywheel");
-        leftslide = hardwareMap.get(DcMotor.class, "leftslide");
-        rightslide = hardwareMap.get(DcMotor.class, "rightslide");
-        arm = hardwareMap.get(DcMotor.class, "arm");
+        flywheel1 = hardwareMap.get(DcMotor.class, "flywheel1");
+        flywheel2 = hardwareMap.get(DcMotor.class, "flywheel2");
 
-        flywheel.setDirection(DcMotor.Direction.FORWARD);
-        leftslide.setDirection(DcMotor.Direction.FORWARD);
-        rightslide.setDirection(DcMotor.Direction.REVERSE);
-        arm.setDirection(DcMotor.Direction.FORWARD);
+        flywheel1.setDirection(DcMotor.Direction.FORWARD);
+        flywheel2.setDirection(DcMotor.Direction.REVERSE);
 
         waitForStart();
         runtime.reset();
 
-        leftslide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightslide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         while (opModeIsActive())
         {
@@ -45,54 +40,11 @@ public class MotorsTest extends LinearOpMode
                 flypower = 0.5;
             }
 
-            flywheel.setPower(Range.clip(flypower, -flypower, flypower));
+            flywheel1.setPower(Range.clip(flypower, -flypower, flypower));
+            flywheel2.setPower(Range.clip(flypower,-flypower,flypower));
 
 
-            if(gamepad1.left_stick_y>0)
-            {
-                leftslide.setTargetPosition(leftslide.getCurrentPosition() + liftincrement);
-                rightslide.setTargetPosition(leftslide.getCurrentPosition() + liftincrement);
 
-                leftslide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                rightslide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-                leftslide.setPower(liftpower);
-                rightslide.setPower(liftpower);
-
-                leftslide.setPower(0);
-                rightslide.setPower(0);
-
-                leftslide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                rightslide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            }
-
-            if(gamepad1.left_stick_y<0 && ((leftslide.getCurrentPosition() - liftincrement) < 0))
-            {
-                leftslide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                rightslide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            }
-            else if(gamepad1.left_stick_y<0)
-            {
-                leftslide.setTargetPosition(leftslide.getCurrentPosition() - liftincrement);
-                rightslide.setTargetPosition(leftslide.getCurrentPosition() - liftincrement);
-
-                leftslide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                rightslide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-                leftslide.setPower(-liftpower);
-                rightslide.setPower(-liftpower);
-
-                leftslide.setPower(0);
-                rightslide.setPower(0);
-
-                leftslide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                rightslide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            }
-
-
-            armpower = gamepad1.right_stick_y;
-
-            arm.setPower(Range.clip(armpower, -armpower, armpower));
         }
 
 
