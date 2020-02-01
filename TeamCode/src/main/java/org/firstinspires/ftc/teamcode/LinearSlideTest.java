87c459a07f7e984d408dabdfb35b6952e23cdac2
kskgit1 <55639416+kskgit1@users.noreply.github.com>
2020-01-31 19:48:24 -0800

    Create LinearSlideTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlideTest.java
@@ -0,0 +1,78 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name = "LinearSlideTest", group = "Linear OpMode")
+public class LinearSlideTest extends LinearOpMode
+{
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftslide, rightslide;
+    double liftpower = gamepad1.left_stick_y;
+    int liftincrement = 500;
+
+    @Override
+    public void runOpMode()
+    {
+
+        leftslide = hardwareMap.get(DcMotor.class, "leftslide");
+        rightslide = hardwareMap.get(DcMotor.class, "rightslide");
+
+        leftslide.setDirection(DcMotor.Direction.FORWARD);
+        rightslide.setDirection(DcMotor.Direction.FORWARD);
+
+        waitForStart();
+        runtime.reset();
+
+        leftslide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightslide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        while (opModeIsActive())
+        {
+            if(gamepad1.left_stick_y>0)
+            {
+                leftslide.setTargetPosition(leftslide.getCurrentPosition() + liftincrement);
+                rightslide.setTargetPosition(leftslide.getCurrentPosition() + liftincrement);
+
+                leftslide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                rightslide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                leftslide.setPower(liftpower);
+                rightslide.setPower(liftpower);
+
+                leftslide.setPower(0);
+                rightslide.setPower(0);
+
+                leftslide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                rightslide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            }
+
+            if(gamepad1.left_stick_y<0 && ((leftslide.getCurrentPosition() - liftincrement) < 0))
+            {
+                leftslide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                rightslide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            }
+            else if(gamepad1.left_stick_y<0)
+            {
+                leftslide.setTargetPosition(leftslide.getCurrentPosition() - liftincrement);
+                rightslide.setTargetPosition(leftslide.getCurrentPosition() - liftincrement);
+
+                leftslide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                rightslide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                leftslide.setPower(-liftpower);
+                rightslide.setPower(-liftpower);
+
+                leftslide.setPower(0);
+                rightslide.setPower(0);
+
+                leftslide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                rightslide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            }
+        }
+    }
+}
